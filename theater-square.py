6099d2bfcd6ae14c1f6c35262b6cbc4a4b004bdc
ETHAN HUNT <ethanmi767@gmail.com>
2026-01-09 07:48:24 +0530

    Day-1

---
+++ theater-square.py
@@ -0,0 +1,8 @@
+n, m, a = map(int, input().split())  # MAP INPUTS IN DIFFERENT VARIABLES
+
+n1 = (n + a - 1) // a  # FLOOR ROUND-OFF OPERATOR
+m1 = (m + a - 1) // a
+
+print(n1 * m1)
+
+# CODEFORCES QUESTION
